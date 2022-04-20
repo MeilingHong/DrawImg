4a230f773cd49188b6e90366de868b9983e08530
MeilingHong <marisareimu@126.com>
2022-04-20 09:19:13 +0800

    初始

--- app/src/main/java/com/meiling/drawimg/MainActivity.java
+++ app/src/main/java/com/meiling/drawimg/MainActivity.java
@@ -3,12 +3,30 @@ package com.meiling.drawimg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private volatile boolean isFinish = false;
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+    private ImageView show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void createImg(View v){
+        //
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
     }
 }
